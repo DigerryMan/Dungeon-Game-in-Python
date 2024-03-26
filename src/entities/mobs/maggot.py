85635c060f7732ac5334ef3248950dc629b0e279
Mateusz Bobula <matbobula@student.agh.edu.pl>
2024-03-26 21:30:44 +0100

    Cleanig: maggot + alphamaggot

--- src/entities/mobs/maggot.py
+++ src/entities/mobs/maggot.py
@@ -9,13 +9,18 @@ from utils.directions import Directions
 class Maggot(Enemy):
     def __init__(self, game, x, y, moving_clockwise=random.choice([True, False])):
         super().__init__(game, x, y, False)
-        self._speed = 3
+        #CHANGEABLE STATS
         self._health = 4
+        self._speed = 3
+        self._random_dir_change_cd = 2000
+        
+        #SKIN
         self.image.fill(WHITE)
+
+        #REST
         self.moving_clockwise = moving_clockwise
         self._last_change_of_direction = pygame.time.get_ticks()
-        self._random_dir_change_cd = 2000
-
+        
     def move(self):
         self.wall_collision()
         self.random_change_of_direction()
@@ -33,7 +38,6 @@ class Maggot(Enemy):
         
     def attack(self):
         pass
-
 
     def wall_collision(self):
         hits = pygame.sprite.spritecollide(self, self.game.collidables, False)
@@ -53,7 +57,6 @@ class Maggot(Enemy):
 
             self.rotate_facing()
             
-
     def random_change_of_direction(self):
         now = pygame.time.get_ticks()
         if now - self._last_change_of_direction > self._random_dir_change_cd:
@@ -70,6 +73,5 @@ class Maggot(Enemy):
         self.roll_rotation_cd(400, 2200)
         self._last_change_of_direction = pygame.time.get_ticks()
         
-
     def roll_rotation_cd(self, mini:int, maxi:int):
         self._random_dir_change_cd = random.randint(mini, maxi)
