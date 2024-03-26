37dd487cb5bb45fa987839861040b5865a4525d1
Mateusz Bobula <matbobula@student.agh.edu.pl>
2024-03-26 21:14:08 +0100

    Cleaning up: legs

--- src/entities/mobs/legs.py
+++ src/entities/mobs/legs.py
@@ -2,7 +2,7 @@ from ..enemy import *
 
 class Legs(Enemy):
     def __init__(self, game, x, y):
-        super().__init__(game, x, y, True, False)
+        super().__init__(game, x, y, True, is_wandering=False)
         self._speed = 2
         self._health = 5
         self.image.fill(PURPLE)
