ffc19fe619e77746304d3dd7970f9b83b0d26f41
Jakub Konopka <jakubkonopka@student.agh.edu.pl>
2024-04-21 16:13:35 +0200

    Item hitbox improvements

--- src/items/stat_items/item.py
+++ src/items/stat_items/item.py
@@ -4,13 +4,19 @@ from items.stat_items.items_list import ItemsList
 class Item(LootableItem):
     def __init__(self, game, x, y, category):
         super().__init__(game, x, y)
-
+        
         self.item = None
 
         self.roll_item(category)
 
+        self.width = game.settings.TILE_SIZE
+        self.height = game.settings.TILE_SIZE
         self.image = self.item["image"]
 
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
 
     def roll_item(self, category):
         self.item = self.game.items_list.commons[0]
