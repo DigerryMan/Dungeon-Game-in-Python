2a05c8266b422d363f04b2606dbdf7b48993952a
Mateusz Bobula <matbobula@student.agh.edu.pl>
2024-04-21 18:11:48 +0200

    big_item showing and merging same items

--- src/entities/player/equipment.py
+++ src/entities/player/equipment.py
@@ -38,7 +38,6 @@ class Equipment():
             "luck": 0.5
         }
 
-
         self.player = player
 
         #background
@@ -48,7 +47,7 @@ class Equipment():
         self.y = (player.game.settings.WIN_HEIGHT - self.height) // 2
 
         #items
-        self.items = [] #dla testu dane
+        self.items = []
         self.first_item_x = self.x + 22
         self.first_item_y = self.y + 22
         self.item_distance = 73
@@ -76,9 +75,6 @@ class Equipment():
         self.big_item_size = 137
         self.big_item_image = None
 
-        #DLA TESTU
-        self.licznik = 0
-
     def draw(self, screen):
         self.draw_background(screen)
         self.draw_items(screen)
@@ -94,13 +90,7 @@ class Equipment():
             x = self.items_x[items_drawn % self.item_in_row]
             y = self.items_y[items_drawn // self.item_in_row]
 
-            #dla testu
-            item_image = pygame.Surface((self.item_size, self.item_size))
-            item_image.fill(config.GREEN)
-
-            #poprawnie
-            #item_image = pygame.transform.scale(item.get_image(), (self.item_size, self.item_size))
-                        
+            item_image = pygame.transform.scale(item["image"], (self.item_size, self.item_size))       
             screen.blit(item_image, (x, y))
 
             items_drawn += 1
@@ -115,21 +105,21 @@ class Equipment():
             screen.blit(self.big_item_image, (self.big_item_x, self.big_item_y))
 
     def user_eq_input(self, event_key):
-        if event_key == pygame.K_DOWN or event_key == pygame.K_s:
-            self.highlighted_item_col += 1
-                    
-        if event_key == pygame.K_UP or event_key == pygame.K_w:
-            self.highlighted_item_col -= 1
-                    
-        if event_key == pygame.K_LEFT or event_key == pygame.K_a:
-            self.highlighted_item_row -= 1
-                    
-        if event_key == pygame.K_RIGHT or event_key == pygame.K_d:
-            self.highlighted_item_row += 1
-        
-        self.highlighted_item_row %= self.item_in_row
-        self.highlighted_item_col %= self.item_in_col
-
+        if event_key is not None:
+            if event_key == pygame.K_DOWN or event_key == pygame.K_s:
+                self.highlighted_item_col += 1
+                        
+            if event_key == pygame.K_UP or event_key == pygame.K_w:
+                self.highlighted_item_col -= 1
+                        
+            if event_key == pygame.K_LEFT or event_key == pygame.K_a:
+                self.highlighted_item_row -= 1
+                        
+            if event_key == pygame.K_RIGHT or event_key == pygame.K_d:
+                self.highlighted_item_row += 1
+            
+            self.highlighted_item_row %= self.item_in_row
+            self.highlighted_item_col %= self.item_in_col
         self.set_cursor_and_big_item()
     
     def set_cursor_and_big_item(self):
@@ -141,26 +131,17 @@ class Equipment():
             self.big_item_image = None
             return
 
-        #self.big_item_image = pygame.transform.scale(self.items[self.highlighted_item_index].get_image(), 
-                            #(self.big_item_size, self.big_item_size))
-        
-        #DLA TESTU
-        self.big_item_image = pygame.Surface((self.big_item_size, self.big_item_size))
-        
-        if self.licznik == 0:
-            self.big_item_image.fill(config.GREEN)
-        
-        if self.licznik == 1:
-            self.big_item_image.fill(config.BLUE)
-        
-        if self.licznik == 2:
-            self.big_item_image.fill(config.RED)
-        
-        self.licznik += 1
-        self.licznik %= 3
+        self.big_item_image = pygame.transform.scale(self.items[self.highlighted_item_index]["image"], 
+                                                    (self.big_item_size, self.big_item_size))
 
     def add_item(self, item):
-        self.items.append(item)
+        try:
+            index = self.items.index(item)
+            self.items[index]["amount"] += 1
+        except ValueError:
+            item["amount"] = 1
+            self.items.append(item)
+
         self.unpack_item(item)
 
     def unpack_item(self, item):
@@ -177,7 +158,3 @@ class Equipment():
         self.player.update_player_stats()
         if healValue:
             self.player.heal(healValue)
-        print(self.stats)  
-
-
-    
